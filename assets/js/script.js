95eba84e9ea3cf7920d913e8115621cc77906dba
Morgan Klockars <morganklockars@gmail.com>
2023-04-03 17:18:48 +0000

    changed layout and abit of javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,108 +1,89 @@
-const question = document.getElementById("question")
-const choices = Array.from(document.querySelectorAll(".choice-text"))
-const progressText = document.querySelector("#progressText")
-const scoreText = document.querySelector("#Score")
-
-let currentQuestion = {}
-let acceptingAnswers = true
-let score = 0 
-let questionsCounter = 0 
-let availableQuestions = []
-
-let questions = [
+const questionElement = document.getElementById("question")
+const answerButtons = document.getElementById("answer-buttons")
+const nextButton = document.getElementById("next-btn")
+const questions = [
     {
         question: "What Movie do we find\n the character Aragorn from,",
-        choice1: "Game Of Thrones",
-        choice2: "Lord Of The Rings",
-        choice3: "Eragon",
-        choice4: "My Little Pony",
-        answer: 2,    
+        answers: [
+            { text: "Game Of Thrones", correct: false},
+            { text: "Lord Of The Rings", correct: true},
+            { text: "Eragon", correct: false},
+            { text: "My Little Pony", correct: false},
+        ]
     },
     {
         question: "what movie did not\n Quentin Tarantino direct",
-        choice1: "Pulp Fiction",
-        choice2: "The Hatefull Eight",
-        choice3: "Django Unchained",
-        choice4: "John Wick", 
-        answer: 4,
+        answers: [
+            { text: "Pulp Fiction", correct: false},
+            { text: "The Hatefull Eight", correct: false},
+            { text: "Django Unchained", correct: false},
+            { text: "John Wick", correct: true},
+        ]
     },
     {
         question: "What movie did Samuel L. Jackson\n not star in",
-        choice1: "Captain Marvel",
-        choice2: "Starwars",
-        choice3: "The Lighthouse",
-        choice4: "Kong: Skull Island", 
-        answer: 3,
+        answers: [
+            { text: "Captain Marvel", correct: false},
+            { text: "Starwars", correct: false},
+            { text: "The Lighthouse", correct: true},
+            { text: "Kong: Skull Island", correct: false},
+        ]
     },
-   
     {
         question: "What Hollywood movie star\n plays himself in Zombieland?",
-        choice1: "Bill Murray",
-        choice2: "Brad Pitt",
-        choice3: "The Rock",
-        choice4: "Emma Watson", 
-        answer: 1,
+        answers: [
+            { text: "Bill Murray", correct: true},
+            { text: "Brad Pitt", correct: false},
+            { text: "The Rock", correct: false},
+            { text: "Emma Watson", correct: false},
+        ]
     },
-    
+
 ]
 
-const SCORE_POINTS = 100
-const MAX_QUESTIONS = 4
+let score = 0
+let currentQuestionI = 0 
 
-
-runGame = () => {
-    questionsCounter = 0
+function startGame(){
     score = 0
-    availableQuestions = [...questions]
-    getNewQuestion()
+    currentQuestionI = 0
+    nextButton.innerHTML = "next"
+    showQuestion()
 }
 
-getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionsCounter > MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score)
+function showQuestion() {
+    resetBtn()
+    let currentQuestion = questions[currentQuestionI]
+    let questionNum = currentQuestionI + 1;
+    questionElement.innerHTML = questionNum + ". " + currentQuestion.question
 
-        return window.location.assign('/end.html')
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.classList.add("btn")
+        button.innerHTML = answer.text
+        answerButtons.appendChild(button)
+        if(answer.correct){
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+    })
+    
+}
+
+function resetBtn(){
+    nextButton.style.display = "none"
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild)
     }
-
-    questionsCounter++
-    progressText.innerHTML = `question ${questionsCounter} of ${MAX_QUESTIONS}` 
-
-    const questionsIdex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestions[questionsIdex]
-    question.innerText = currentQuestion.question
-
-    choices.forEach(choice => {
-        const number = choice.dataset['number']
-        choice.innerText = currentQuestion['choice' + number]
-    })
-
-    availableQuestions.splice(questionsIdex, 1)
-
-    acceptingAnswers = true
 }
 
-choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return
-
-        acceptingAnswers = false
-        const selectedChoice = e.target
-        const selectedAnswer = selectedChoice.dataset['number']
-
-        let classToAplly = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'       
-
-        selectedChoice.parentElement.classList.add(classToAplly)
-
-        setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToAplly)
-            getNewQuestion()
-        }, 1000)
-    })
-})
-
-incrementScore = num => {
-    score +=num
-    scoreText.innerText = score
+function selectAnswer(e){
+    const selectedBtn = e.target
+    const isCorrect = selectedBtn.dataset.correct === "true"
+    if(isCorrect){
+        selectedBtn.classList.add("correct")
+    }else{
+        selectedBtn.classList.add("incorrect")
+    }
 }
-
-runGame()
+startGame()
