182cf36f7edd8bc144260f3a79921c1135f0a346
Morgan Klockars <morganklockars@gmail.com>
2023-04-03 15:55:41 +0000

    Made the project smaller and removing highscore page and end page realised i would not have time

--- assets/js/highscores.js
+++
@@ -1,6 +0,0 @@
-const highScoresList = document.querySelector('#highscoresList')
-const highScores = JSON.parse(localStorage.getItem('highScores')) || []
-
-highScoresList.innerHTML = highScores.map(score => {
-    return `<li class="high-score>${score.name} - ${score.score}`
-}).join('')
